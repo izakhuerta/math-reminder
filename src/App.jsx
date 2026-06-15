ba1474f17125e4b2926388e7b39f62e4b152e7af
izakhuerta <izakhuerta97@gmail.com>
2026-06-15 14:38:41 -0500

    zxc

--- src/App.jsx
+++ src/App.jsx
@@ -192,7 +192,7 @@ export default function App() {
 
   useEffect(() => {
     const savedPoints = Number(
-      localStorage.getItem("points") || 67
+      localStorage.getItem("points") || 0
     );
 
     const savedStreak = Number(
